3d363583731deca70a0513004c83302ac6748ce1
Ruslan Prokopchuk <fer.obbee@gmail.com>
2015-05-14 15:13:49 +0300

    make separate project for simple example

--- project.clj
+++ project.clj
@@ -1,69 +1,14 @@
 (defproject ampere "0.1.0-SNAPSHOT"
-  :description "FIXME: write this!"
-  :url "http://example.com/FIXME"
+  :description "Ampere: UniDirectional Flow for VDOM."
+  :url "https://github.com/ul/ampere"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.7.0-beta3"]
-                 [org.clojure/clojurescript "0.0-3269"]
+                 [org.clojure/clojurescript "0.0-3269" :scope "provided"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [org.clojure/core.match "0.3.0-alpha4" :scope "test"]
-                 [sablono "0.3.4"]
-                 [org.omcljs/om "0.8.8"]
                  [tailrecursion/javelin "3.8.0"]
-                 [vfsm "0.1.0-SNAPSHOT"]]
-
-  :plugins [[lein-cljsbuild "1.0.5"]
-            [lein-figwheel "0.3.1"]]
-
-  :source-paths ["src"]
-
-  :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
-  
-  :cljsbuild {
-    :builds [{:id "dev"
-              :source-paths ["src" "examples"]
-
-              :figwheel { :on-jsload "ampere.example/on-js-reload" }
-
-              :compiler {:main ampere.example
-                         :asset-path "js/compiled/out"
-                         :output-to "resources/public/js/compiled/ampere.js"
-                         :output-dir "resources/public/js/compiled/out"
-                         :source-map-timestamp true }}
-             {:id "min"
-              :source-paths ["src" "examples"]
-              :compiler {:output-to "resources/public/js/compiled/ampere.js"
-                         :main ampere.example
-                         :optimizations :advanced
-                         :pretty-print false}}]}
-
-  :figwheel {
-             :http-server-root "public" ;; default and assumes "resources"
-             :server-port 3449 ;; default
-             :css-dirs ["resources/public/css"] ;; watch and update CSS
-
-             ;; Start an nREPL server into the running figwheel process
-             :nrepl-port 7888
-
-             ;; Server Ring Handler (optional)
-             ;; if you want to embed a ring handler into the figwheel http-kit
-             ;; server, this is for simple ring servers, if this
-             ;; doesn't work for you just run your own server :)
-             ;; :ring-handler hello_world.server/handler
-
-             ;; To be able to open files in your editor from the heads up display
-             ;; you will need to put a script on your path.
-             ;; that script will have to take a file path and a line number
-             ;; ie. in  ~/bin/myfile-opener
-             ;; #! /bin/sh
-             ;; emacsclient -n +$2 $1
-             ;;
-             ;; :open-file-command "myfile-opener"
-
-             ;; if you want to disable the REPL
-             ;; :repl false
-
-             ;; to configure a different figwheel logfile path
-             ;; :server-logfile "tmp/logs/figwheel-logfile.log" 
-             })
+                 [vfsm "0.1.0-SNAPSHOT"]
+                 ;; Om
+                 [sablono "0.3.4"]
+                 [org.omcljs/om "0.8.8"]])
