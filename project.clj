16a4c192158491b613c093a0b8acd50b883f1341
Ruslan Prokopchuk <fer.obbee@gmail.com>
2015-06-16 12:32:58 +0300

    up deps

--- project.clj
+++ project.clj
@@ -7,6 +7,6 @@
   :dependencies [[org.clojure/clojure "1.7.0-RC1"]
                  [org.clojure/clojurescript "0.0-3308" :scope "provided"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [ampere/freactive.core "0.2.0"]
+                 [ampere/freactive.core "0.2.1"]
                  [org.omcljs/om "0.8.8" :scope "provided"]
                  [reagent "0.5.0" :scope "provided"]])
