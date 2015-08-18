e4ddf7019a804c6cb86461b4252a4ed8e7be37b8
Ruslan Prokopchuk <fer.obbee@gmail.com>
2015-08-18 14:21:12 +0300

    invalidate subs cache, do not cache sample

--- src/ampere/subs.cljs
+++ src/ampere/subs.cljs
@@ -6,40 +6,49 @@
             [ampere.utils :refer [first-in-vector warn error]]))
 
 (def ^:private key->fn "handler-id → handler-fn" (atom {}))
-
-(def ^:private cache (atom {}))
+(def ^:private cache "[db v] → rx" (atom {}))
+(def ^:dynamic *cache?* true)
 
 (defn clear-handlers!
   "Unregisters all subscription handlers."
   []
+  (reset! cache {})
   (reset! key->fn {}))
+
+(defn remove-keys [pred m]
+  (if m (reduce-kv (fn [m k _] (if (pred k) (dissoc m k) m)) m m) {}))
 
 (defn register
   "Registers a handler function for an id."
   [key-v handler-fn]
   (if (contains? @key->fn key-v)
     (warn "ampere: overwriting subscription-handler for: " key-v))   ;; allow it, but warn.
+  (swap! cache (partial remove-keys (comp (partial = key-v) first second))) ; [db [key-v ...]]
   (swap! key->fn assoc key-v handler-fn))
 
 (defn path-handler [db v]
   (rx (get-in @db v)))
 
 (defn subscribe
-  "Returns a reaction which observes a part of app-db.
-  FIXME allow static subscriptions, wrap them to be not disposed by adapter."
+  "Returns a reaction which observes a part of app-db."
   ([v] (subscribe app-db v))
   ([db v]
    (let [key-v (first-in-vector v)
          handler-fn (get @key->fn key-v path-handler)
          cache-key [db v]]
-     (if-let [sub (get @cache cache-key)]
-       sub
-       (let [sub (handler-fn db v)
-             sub (freactive.core/rx* #(deref sub) true #(swap! cache dissoc cache-key))]
+     (if *cache?*
+       (if-let [sub (get @cache cache-key)]
+         sub
+         (let [sub (handler-fn db v)
+               sub (freactive.core/rx* #(deref sub) true #(swap! cache dissoc cache-key))]
+           (obj/set sub "__ampere_v" v)
+           (swap! cache assoc cache-key sub)
+           sub))
+       (let [sub (handler-fn db v)]
          (obj/set sub "__ampere_v" v)
-         (swap! cache assoc cache-key sub)
          sub)))))
 
 (defn sample [db v]
   "Sample subscription against immutable db value."
-  @(subscribe (freactive.core/atom db) v))
+  (binding [*cache?* false]
+    @(subscribe (freactive.core/atom db) v)))
