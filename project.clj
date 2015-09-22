5b0ad5537216f2dd1185b3ea4909083e5dac1180
Ruslan Prokopchuk <fer.obbee@gmail.com>
2015-09-22 09:40:16 +0300

    downgrade clojurescript dependency to the latest release

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.122" :scope "provided"]
+                 [org.clojure/clojurescript "1.7.48" :scope "provided"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [condense/freactive.core "0.2.2"]
                  [org.omcljs/om "0.9.0" :scope "provided"]
