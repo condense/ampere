b8f29ab0cc0fb23d3a941f051bda0055a15e85c2
Ruslan Prokopchuk <fer.obbee@gmail.com>
2015-09-23 13:16:28 +0300

    use codeina for docs generation

--- project.clj
+++ project.clj
@@ -5,8 +5,15 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.48" :scope "provided"]
+                 [org.clojure/clojurescript "1.7.122" :scope "provided"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [condense/freactive.core "0.2.2"]
                  [org.omcljs/om "0.9.0" :scope "provided"]
-                 [reagent "0.5.1" :scope "provided"]])
+                 [reagent "0.5.1" :scope "provided"]]
+
+  :plugins [[funcool/codeina "0.3.0" :exclusions [org.clojure/clojure]]]
+
+  :codeina {:sources ["src"]
+            :reader :clojurescript
+            :target "docs/api"
+            :defaults {:doc/format :markdown}})
