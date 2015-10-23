b05d76fd03bbcdda2367fafe531280bcf0588590
Ruslan Prokopchuk <fer.obbee@gmail.com>
2015-10-23 14:36:19 +0300

    fix fmt

--- src/ampere/reagent.cljs
+++ src/ampere/reagent.cljs
@@ -5,11 +5,11 @@
             [reagent.ratom :refer [make-reaction]]))
 
 (defn subscribe [v]
-    (let [sub (ampere/subscribe v)
-          a (r/atom @sub)
-          id (gensym)]
-      (add-watch sub id #(reset! a %4))
-      (make-reaction #(deref a) :on-dispose #(remove-watch sub id))))
+  (let [sub (ampere/subscribe v)
+        a (r/atom @sub)
+        id (gensym)]
+    (add-watch sub id #(reset! a %4))
+    (make-reaction #(deref a) :on-dispose #(remove-watch sub id))))
 
 (defn init! []
-    (set! router/*flush-dom* r/flush))
+  (set! router/*flush-dom* r/flush))
