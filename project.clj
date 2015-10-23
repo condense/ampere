6dcd4ee86669a945305c1512b1d77c390ec87335
Ruslan Prokopchuk <fer.obbee@gmail.com>
2015-10-23 09:33:27 +0300

    cut 0.3.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject condense/ampere "0.3.0-SNAPSHOT"
+(defproject condense/ampere "0.3.0"
   :description "Ampere: UniDirectional Flow for VDOM."
   :url "https://github.com/condense/ampere"
   :license {:name "Eclipse Public License"
