02e9a711b57c06b63249d11c1aeb8a9279600958
Ruslan Prokopchuk <fer.obbee@gmail.com>
2016-03-26 11:20:31 +0200

    start new dev cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject condense/ampere "0.3.4"
+(defproject condense/ampere "0.3.5-SNAPSHOT"
   :description "Ampere: UniDirectional Flow for VDOM."
   :url "https://github.com/condense/ampere"
   :license {:name "Eclipse Public License"
