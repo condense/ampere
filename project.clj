d54b4e636691ad61db67e11a80ccfe23f49303f2
Ruslan Prokopchuk <fer.obbee@gmail.com>
2016-06-24 09:31:01 +0300

    cut 0.4.0 and up deps

--- project.clj
+++ project.clj
@@ -1,20 +1,20 @@
-(defproject condense/ampere "0.4.0-SNAPSHOT"
+(defproject condense/ampere "0.4.0"
   :description "Ampere: UniDirectional Flow for VDOM."
   :url "https://github.com/condense/ampere"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.8.40" :scope "provided"]
+                 [org.clojure/clojurescript "1.9.93" :scope "provided"]
                  [org.omcljs/om "0.9.0" :scope "provided"]
                  [reagent "0.5.1" :scope "provided"]
                  [carbon/rx "0.1.9"]]
 
   :profiles {:debug {:debug true}
-             :dev   {:dependencies [[karma-reporter "0.3.0"]]
-                     :plugins      [[lein-cljsbuild "1.1.1"]
-                                    [lein-npm "0.6.1"]
-                                    [lein-figwheel "0.3.8"]]}}
+             :dev   {:dependencies [[karma-reporter "1.0.1"]]
+                     :plugins      [[lein-cljsbuild "1.1.3"]
+                                    [lein-npm "0.6.2"]
+                                    [lein-figwheel "0.5.4-4"]]}}
 
   :clean-targets [:target-path
                   "run/compiled/demo"]
@@ -23,7 +23,7 @@
 
   :jvm-opts       ["-Xmx1g" "-XX:+UseConcMarkSweepGC"]
 
-  :plugins [[funcool/codeina "0.3.0" :exclusions [org.clojure/clojure]]]
+  :plugins [[funcool/codeina "0.4.0" :exclusions [org.clojure/clojure]]]
 
   :source-paths ["src"]
 
